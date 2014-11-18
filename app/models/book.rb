497698c4c6bf482b6a2ce9c704bfa531736824dd
TReMeR <pumpedgeek@gmail.com>
2014-11-18 22:06:59 +0200

    Add recent scope to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
+	scope :recent, -> { where('finished_on > ?', 2.days.ago) }
+	scope :finished, -> { where.not(finished_on: nil) }
 	def finished?
 		finished_on.present?
 	end
