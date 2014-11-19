45b5d3abfe13993ed788b49ca41be1df8817d1c8
TReMeR <pumpedgeek@gmail.com>
2014-11-19 18:31:06 +0200

    Implement simple books search.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ActiveRecord::Base
 	scope :recent, -> { where('finished_on > ?', 2.days.ago) }
 	scope :finished, -> { where.not(finished_on: nil) }
+  scope :search, ->(keyword){where(title: keyword) if keyword.present? }	
+
 	def finished?
 		finished_on.present?
 	end
