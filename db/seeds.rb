4ef5898e7b3b43ea40c824ed4439f24049a1fae7
TReMeR <pumpedgeek@gmail.com>
2014-11-29 21:33:12 +0200

    Modify restaurants names and images

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,29 @@ bar = Genre.create!(name: "Bar")
 club = Genre.create!(name: "Night Club")
 
 Book.create!([{
-  title: "Мафия",
-  author: "Семейный ресторан",
+  title: "Mafia",
+  author: "мафия",
   description: "Probably my favorite science fiction book (and series) I've ever read. Hyperion is written in a style similar to The Canterbury Tales, in which a series of stories are told by the main characters. Each story is a gem in itself, but alude to the larger storyline. The scope of the story is ambitious - spanning time, planets religion and love.",
   rating: 5,
   genres: [restaurant]
 },
 {
-  title: "Водка Бар",
-  author: "Отдыхай весело",
+  title: "Vodka Bar",
+  author: "водка бар",
   description: "Even though I respect Ive, I felt this biography only hit skin deep. It went over all the major events in his life, his passion for design, awards he achieved -- but that's really it. I dont't feel I know him anymore than before reading this.",
   rating: 4,
   genres: [club, bar, cafe]
 },
 {
-  title: "Мама Мия",
-  author: "Лучшая пицца",
+  title: "Mama Mia",
+  author: "мама мия",
   description: "Although this book focuses on the cognitive advantages to having children use technology from an early age, it is also an in depth look at how people can learn for themseves. As someone who was often distracted and bored at times during school, Mindstorms highlights some of the reasoning behind that feeling and what we can do as teachers to help minimize it.",
   rating: 3,
   genres: [cafe, restaurant]
 },
 {
-  title: "СушиЯ",
-  author: "Частичка Японии",
+  title: "SushiYa",
+  author: "сушия",
   description: "Although this book focuses on the cognitive advantages to having children use technology from an early age, it is also an in depth look at how people can learn for themseves. As someone who was often distracted and bored at times during school, Mindstorms highlights some of the reasoning behind that feeling and what we can do as teachers to help minimize it.",
   rating: 4,
   genres: [cafe, restaurant]
